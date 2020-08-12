fb71be7b4f44a7ad827928df4473699dbfa6dcca
Lamia Haider <lamia.haider@gmail.com>
2020-08-12 02:55:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,15 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.create_table
+
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+    SQL
+    
   end
 
 
